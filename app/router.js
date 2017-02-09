adf5edb71204ad6f2b6a04dc86817d1221c807d5
Stephanie Andrews <seandrews11@gmail.com>
2017-02-09 15:22:54 -0700

    display individual reminders on dynamic reminder route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,13 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+<<<<<<< HEAD
   this.route('reminders');
+=======
+  this.route('reminders', { path: '/reminders' }, function() {
+    this.route('reminder', {path: '/reminder/:reminder_id'});
+  });
+>>>>>>> 49e50aa... display individual reminders above reminders list on dynamic reminder route
 });
 
 
---
+++ app/routes/reminders/reminder.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.get('store').findRecord('reminder', params.reminder_id);
+  }
+});
+
+
+//Object.data.id
