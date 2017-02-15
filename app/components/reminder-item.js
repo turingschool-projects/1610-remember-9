f0006866ed3f60c49873b3c3f7bda3f1421a7337
Stephanie Andrews <seandrews11@gmail.com>
2017-02-15 15:09:27 -0700

    Add delete reminder functionality

--- app/components/reminder-item.js
+++ app/components/reminder-item.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  store: Ember.inject.service(),
+
+  actions: {
+    deleteReminder() {
+      this.get('store').findRecord('reminder', this.reminder.id).then(reminder => {
+        reminder.destroyRecord()
+      })
+    }
+  }
 });
